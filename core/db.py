6f998561b978c5f20903d36b0b34dc46ff1c2ac9
Andrei05231 <carare.andrei57@gmail.com>
2025-10-20 13:37:41 +0300

    add mysql trigger to save device user

--- core/db.py
+++ core/db.py
@@ -23,32 +23,32 @@ class OCSDatabase:
     def fetch_hardware_with_gpus(self) -> pd.DataFrame:
         query = """
             SELECT 
-                h.NAME AS hardware_name,
-                    h.USERID AS connected_user,
-                        b.SMODEL AS model_name,
-                            cpu.cpu_type,
-                                h.DESCRIPTION AS device_name,
-                                    gpu.gpus,
-                                        h.IPADDR AS ip,
-                                            mem.memory_modules
-                                            FROM hardware h
-                                            LEFT JOIN bios b ON b.HARDWARE_ID = h.ID
-                                            LEFT JOIN (
-                                                        SELECT HARDWARE_ID, GROUP_CONCAT(TYPE ORDER BY ID SEPARATOR ', ') AS cpu_type
-                                                            FROM cpus
-                                                                GROUP BY HARDWARE_ID
-                                                                ) cpu ON cpu.HARDWARE_ID = h.ID
-                                            LEFT JOIN (
-                                                        SELECT HARDWARE_ID, GROUP_CONCAT(NAME ORDER BY ID SEPARATOR ', ') AS gpus
-                                                            FROM videos
-                                                                GROUP BY HARDWARE_ID
-                                                                ) gpu ON gpu.HARDWARE_ID = h.ID
-                                            LEFT JOIN (
-                                                        SELECT HARDWARE_ID, GROUP_CONCAT(COALESCE(CAPACITY,0) ORDER BY ID SEPARATOR ' ') AS memory_modules
-                                                            FROM memories
-                                                                GROUP BY HARDWARE_ID
-                                                                ) mem ON mem.HARDWARE_ID = h.ID
-                                            WHERE h.NAME REGEXP '^[Aa][Rr][Tt][0-9]{3}$'
-                                            ORDER BY h.NAME;
-                                            """
-        return pd.read_sql(query, self.conn)
+                h.NAME AS hardware_name,=
+                h.USERID AS connected_user,
+                b.SMODEL AS model_name,
+                cpu.cpu_type,
+                h.DESCRIPTION AS device_name,
+                gpu.gpus,
+                h.IPADDR AS ip,
+                mem.memory_modules
+                FROM hardware h
+                LEFT JOIN bios b ON b.HARDWARE_ID = h.ID
+                LEFT JOIN (
+                            SELECT HARDWARE_ID, GROUP_CONCAT(TYPE ORDER BY ID SEPARATOR ', ') AS cpu_type
+                                FROM cpus
+                                    GROUP BY HARDWARE_ID
+                                    ) cpu ON cpu.HARDWARE_ID = h.ID
+                LEFT JOIN (
+                            SELECT HARDWARE_ID, GROUP_CONCAT(NAME ORDER BY ID SEPARATOR ', ') AS gpus
+                                FROM videos
+                                    GROUP BY HARDWARE_ID
+                                    ) gpu ON gpu.HARDWARE_ID = h.ID
+                LEFT JOIN (
+                            SELECT HARDWARE_ID, GROUP_CONCAT(COALESCE(CAPACITY,0) ORDER BY ID SEPARATOR ' ') AS memory_modules
+                                FROM memories
+                                    GROUP BY HARDWARE_ID
+                                    ) mem ON mem.HARDWARE_ID = h.ID
+                WHERE h.NAME REGEXP '^[Aa][Rr][Tt][0-9]{3}$'
+                ORDER BY h.NAME;
+                """
+        return pd.rad_sql(query, self.conn)
